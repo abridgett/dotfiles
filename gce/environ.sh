73ff3fe331608a1faa5fba222a212b725b2d58d2
Adrian Bridgett <adrian@smop.co.uk>
2015-01-26 09:47:44 +0000

    add brew compatibility

--- gce/environ.sh
+++ gce/environ.sh
@@ -1,3 +1,7 @@
-if [ -f /usr/local/google-cloud-sdk/path.bash.inc ]; then
+if [ "$ZSH" ] && [ -f /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc ]; then
+  # Cannot source this as anto-ls breaks it
+  export PATH=$PATH:/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+elif [ -f /usr/local/google-cloud-sdk/path.bash.inc ]; then
   . /usr/local/google-cloud-sdk/path.bash.inc
 fi
