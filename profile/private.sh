afaeb44dce3a2210aeeb67a6738cab8805c5ba53
Adrian Bridgett <adrian@smop.co.uk>
2015-01-26 09:49:33 +0000

    ansible/gce variables

--- profile/private.sh
+++ profile/private.sh
@@ -4,3 +4,5 @@ export IRCSERVER=irc.blitzed.org
 export EMAIL=adrian@smop.co.uk
 export ORGANIZATION='Bitcube Ltd.'
 
+export SSL_CERT_FILE=$HOME/.gce/cacert.pem
+export ANSIBLE_HOST_KEY_CHECKING=False
