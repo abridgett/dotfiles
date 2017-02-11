394c415f89d966d0b06f47b975501f4779f7c7fa
Adrian Bridgett <adrian@smop.co.uk>
2017-02-11 15:33:16 +0000

    remove custom ansible

--- ansible/environ.sh
+++
@@ -1,7 +0,0 @@
-# Ansible
-export PATH=$PATH:$PROJECTS/ansible/bin
-# Beware: Brew's ansible binaries override this
-#export PYTHONPATH=$PROJECTS/ansible/lib:$HOME/.gce/secrets.py
-export ANSIBLE_LIBRARY=$PROJECTS/ansible/library:/usr/share/ansible/
-export MANPATH=$PROJECTS/ansible/docs/man:
-export GCE_INI_PATH=$HOME/.gce/gce.ini
