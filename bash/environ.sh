216212573be4351fad5751efb02499881a3e687d
Adrian Bridgett <adrian@smop.co.uk>
2017-12-10 22:29:02 +0000

    bigger history

--- bash/environ.sh
+++ bash/environ.sh
@@ -1,4 +1,4 @@
-export HISTSIZE=200            # Number of lines in history file.
+export HISTSIZE=2000           # Number of lines in history file.
 export HISTCONTROL=ignoreboth  # uniq history and ignore spaces
 export HISTIGNORE="&:bg:exit"  # ignore duplicate commands
 export HISTTIMEFORMAT="%H:%M:%S %d/%m/%Y "
